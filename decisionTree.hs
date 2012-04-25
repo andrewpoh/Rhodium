94b292e6a709a173f79354c1248ce346f4b0721f
AndrewPoh <andrewpoh@electronpoetry.com>
2012-04-25 01:09:53 -0700

    added todos

--- decisionTree.hs
+++ decisionTree.hs
@@ -13,3 +13,8 @@ runTree frame index (DtSplit matcher trueBranch falseBranch) =
 	if matcher frame index
 		then runTree frame index trueBranch
 		else runTree frame index falseBranch
+
+-- generate Matchers
+-- calculate improvement
+-- determine stopping condition (mostly pure, few samples, no attributes)
+-- Use (weighted) squared deviation for node impurity
