f9cbccd78e5e2cdbd84f75a1ad622893ff52a957
AndrewPoh <andrewpoh@electronpoetry.com>
2012-04-26 00:50:18 -0700

    starting on decision tree training

--- decisionTree.hs
+++ decisionTree.hs
@@ -16,6 +16,11 @@ runTree frame index (DtSplit matcher trueBranch falseBranch) =
 		then runTree frame index trueBranch
 		else runTree frame index falseBranch
 
+scoreSplitter :: Matcher m => Dataframe -> [Int] -> m -> Double
+scoreSplitter frame indices m =
+	let (trues, falses) = partition (matchOne m frame) indices in
+	??
+
 squaredDeviation :: [Double] -> [Double] -> Double
 squaredDeviation actual prediction =
 	sum $ zipWith (\x y-> let d = x - y in d * d) actual prediction
