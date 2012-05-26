830ada30764998f4229c9af948bc8c5e19602e6c
AndrewPoh <andrewpoh@electronpoetry.com>
2012-05-26 12:50:11 -0700

    added int grouping discretiser

--- Rhodium/Segment/discretisers.hs
+++ Rhodium/Segment/discretisers.hs
@@ -3,6 +3,7 @@ module Rhodium.Segment.Discretisers
 	where
 
 import qualified Data.Array.Unboxed as A
+import qualified Data.Map as M
 import Rhodium.Data.DataCell
 import Rhodium.Data.DataColumn
 import Rhodium.Data.Dataframe
@@ -12,6 +13,18 @@ class Discretiser k where
 	discretiseSingle :: k -> Dataframe -> Int -> DiscType k
 	discretiseMany :: k -> Dataframe -> [Int] -> [DiscType k]
 	discretiseMany k f = map (discretiseSingle k f)
+
+data IntGrouping = IntGrouping Name (M.Map Int Int) Int
+instance Discretiser IntGrouping where
+	type DiscType IntGrouping = Int
+	discretiseSingle (IntGrouping n mappings defaultLevel) f ix =
+		let datum = getIntColumn f n A.! ix in
+		M.findWithDefault defaultLevel datum mappings
+	discretiseMany (IntGrouping n mappings defaultLevel) f ixs =
+		let columnArray = getIntColumn f n in
+		map (\ix ->
+			M.findWithDefault defaultLevel (columnArray A.! ix) mappings)
+			ixs
 
 data ShowDisc = ShowDisc Name
 instance Discretiser ShowDisc where
